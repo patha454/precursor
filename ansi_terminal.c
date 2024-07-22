920ce6ec1eb46a62e31bf1b9450d6e4fc17090ad
H Paterson <26947190+patha454@users.noreply.github.com>
2024-07-22 22:21:34 +0100

    Feature: Rewrite demo program

---
+++ ansi_terminal.c
@@ -0,0 +1,70 @@
+/**
+* \file ansi_terminal.c
+ *
+ * Implementation of ANSI terminal control codes.
+ *
+ * \author H Paterson.
+ * \copyright BSL-1.0.
+ */
+
+
+#include "terminal.h"
+
+static void ansiSetForeground(FILE* terminal, const pc_colour_t colour)
+{
+    fprintf(
+        terminal,
+        "\x1b[38;2;%d;%d;%dm",
+        colour.red,
+        colour.green,
+        colour.blue
+    );
+}
+
+static void ansiSetBackground(FILE* terminal, const pc_colour_t colour)
+{
+    fprintf(
+        terminal,
+        "\x1b[48;2;%d;%d;%dm",
+        colour.red,
+        colour.green,
+        colour.blue
+    );
+}
+
+static void ansiSetBold(FILE* terminal, const bool bold)
+{
+    if (bold)
+    {
+        fputs("\x1b[1m", terminal);
+    } else
+    {
+        fputs("\x1b[22m", terminal);
+    }
+}
+
+static void ansiSetItalic(FILE* terminal, const bool enable)
+{
+    if (enable)
+    {
+        fputs("\x1b[3m", terminal);
+    } else
+    {
+        fputs("\x1b[23m", terminal);
+    }
+}
+
+static void ansiReset(FILE* terminal)
+{
+    const char* code = "\x1b[0m";
+    fputs(code, terminal);
+}
+
+const struct PcTerminal ansiTerminal =
+{
+    .setForeground = ansiSetForeground,
+    .setBackground = ansiSetBackground,
+    .enableBold = ansiSetBold,
+    .enableItalic = ansiSetItalic,
+    .reset = ansiReset,
+};
---
+++ ansi_terminal.h
@@ -0,0 +1,8 @@
+//
+// Created by Harley on 7/22/2024.
+//
+
+#ifndef ANSI_TERMINAL_H
+#define ANSI_TERMINAL_H
+
+#endif //ANSI_TERMINAL_H
