c00e59ca98c34c29da4ebceff9622c1c1cb1fd1d
H Paterson <26947190+patha454@users.noreply.github.com>
2024-08-08 21:42:29 +0100

    Tests: Test `colour.c`

--- src/colour.c
+++ src/colour.c
@@ -1,6 +1,7 @@
 #include "colour.h"
 
-pc_colour_t PcColour(const uint8_t red, const uint8_t green, const uint8_t blue)
+extern pc_colour_t PcColour(
+    const uint8_t red, const uint8_t green, const uint8_t blue)
 {
     const pc_colour_t colour = { .red = red, .blue = blue, .green = green };
     return colour;
--- test/precursor_tests.c
+++ test/precursor_tests.c
@@ -7,12 +7,26 @@
 #include "cmocka.h"
 // clang-format on
 
+#include "colour.h"
+
+static void test_pc_colour(void** state)
+{
+    (void)state;
+    const uint8_t red = 123;
+    const uint8_t green = 33;
+    const uint8_t blue = 255;
+    pc_colour_t colour = PcColour(red, green, blue);
+    assert_int_equal(colour.red, red);
+    assert_int_equal(colour.green, green);
+    assert_int_equal(colour.blue, blue);
+}
+
 /* A test case that does nothing and succeeds. */
 static void null_test_success(void** state) { (void)state; /* unused */ }
 int main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(null_test_success),
+        cmocka_unit_test(test_pc_colour),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
